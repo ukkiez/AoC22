67ef2f81b8502a370a24a8b0b40fab76e8e790f2
ukkiez <yannick_3d@hotmail.com>
2022-12-05 11:27:38 +0100

    day5 JS

---
+++ js/day5/index.js
@@ -0,0 +1,67 @@
+const InputParser = require( "../InputParser.js" );
+
+const main = async () => {
+  let answer1 = "";
+  let answer2 = "";
+
+  const { lines } = await InputParser.init( __dirname, "./input.txt" );
+
+  // get the initial arrangement, from bottom to top (find the bottom by the
+  // empty new line)
+  const index = lines.findIndex( e => !e );
+  const stacks = new Map();
+  const stacksNumber = parseInt( lines[ index-1 ].match( /\d+$/ )[ 0 ], 10 )
+  for ( let i = 0; i < stacksNumber; i++ ) {
+    stacks.set( i, [] );
+  }
+
+  let i = index-2;
+  while ( i >= 0 ) {
+    const chars = [ ...lines[ i ] ];
+    for ( let l = 0; l < chars.length; l++ ) {
+      if ( chars[ l ] === " " ) {
+        continue;
+      }
+
+      if ( chars[ l ] === "[" ) {
+        const stackIndex = ( l / 4 );
+        stacks.get( stackIndex ).push( chars[ l+1 ] );
+        l++;
+      }
+    }
+
+    i--;
+  }
+
+  // deep clone the existing map
+  const stacks2 = new Map( JSON.parse(
+    JSON.stringify( Array.from( stacks ) )
+  ) );
+
+  for ( let i = index+1; i < lines.length-1; i++ ) {
+    const [ number, from, to ] = lines[ i ].match( /\d+/g ).map( Number );
+    // part 1
+    const source = stacks.get( from-1 );
+    const destination = stacks.get( to-1 );
+    for ( let n = 0; n < number; n++ ) {
+      destination.push( source.pop() );
+    }
+
+    // part 2
+    const crates = stacks2.get( from-1 ).splice( stacks2.get( from-1 ).length - number );
+    stacks2.set( to-1, stacks2.get( to-1 ).concat( crates ) );
+  }
+
+  for ( const value of stacks.values() ) {
+    answer1 += value[ value.length-1 ];
+  }
+  for ( const value of stacks2.values() ) {
+    answer2 += value[ value.length-1 ];
+  }
+
+  return { answer1, answer2 };
+}
+
+main().then( result => {
+  console.log( result );
+} );
